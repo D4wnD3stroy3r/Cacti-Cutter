d0eb2a9207a60298cade0b60604871a8693b0622
D4wnD3stroy3r <101806886+D4wnD3stroy3r@users.noreply.github.com>
2024-11-01 22:57:50 -0400

    Organized and commented code

--- main.gd
+++ main.gd
@@ -13,6 +13,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+# Fills the mouse_position_stack array with the last 5 points the mouse has traveled
 func _input(event):
 	if event is InputEventMouseMotion:
 		if event.relative:
@@ -22,14 +23,24 @@ func _input(event):
 				#print(mouse_position_stack)
 
 func _on_cactus_target_mouse_exited():
+	# Add score and update score in ScoreBox
 	score += 1
 	$CactusTarget/ScoreBox.text = str(score)
-	var particle_angle = mouse_position_stack[0].angle_to_point(get_viewport().get_mouse_position())
 	
+	# Create a new instance of the cut_particle scene
 	var particle = cut_particle.instantiate()
-	var velocity = Vector2(200,0)
-	var particle_spawn_location = get_viewport().get_mouse_position()
-	particle.position = particle_spawn_location
+	
+	# Set the spawn position of the particle to the mouse position when the mouse exists the collision box
+	var mouse_position_on_exit = get_viewport().get_mouse_position()
+	particle.position = mouse_position_on_exit
+	
+	# Set the rotation of the spawned particle to the angle of the mouse when it exists the collision box
+	var particle_angle = mouse_position_stack[0].angle_to_point(mouse_position_on_exit)
 	particle.rotation = particle_angle
+	
+	# Set the velocity of the particle
+	var velocity = Vector2(200,0)
 	particle.linear_velocity = velocity.rotated(particle_angle)
+	
+	# Spawn the particle
 	add_child(particle)
