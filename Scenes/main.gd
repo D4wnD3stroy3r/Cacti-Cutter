952269802478244aab4031fb63695307fc52fc51
D4wnD3stroy3r <101806886+D4wnD3stroy3r@users.noreply.github.com>
2024-11-06 01:37:42 -0500

    Organized scenes into a folder along with associated scripts

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -1,22 +1,25 @@
 extends Node
 
-var cut_particle = load("res://cut_particle.tscn")
-var mouse_position_stack = []
+var cut_particle = load("res://Scenes/cut_particle.tscn")
+var mouse_position_stack : Array
+var mouse_velocity : Vector2
 var saveData = SaveData.new()
 var save_file = "user://save.tres" # %appdata%\Roaming\Godot\app_userdata\Cacti Cutter\save.tres
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Initiate ScoreBox value
-	$CactusTarget/ScoreBox.text = str(0)
+	$CactusTarget/ScoreBox.text = str(saveData.score)
 	if FileAccess.file_exists(save_file):
 		load_game()
+	$SaveTimer.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	# Update ScoreBox value every frame
 	$CactusTarget/ScoreBox.text = str(saveData.score)
+
 
 # Fills the mouse_position_stack array with the last 5 points the mouse has traveled
 func _input(event):
@@ -25,7 +28,6 @@ func _input(event):
 			mouse_position_stack.append(get_viewport().get_mouse_position())
 			if mouse_position_stack.size() > 5:
 				mouse_position_stack.remove_at(0)
-				#print(mouse_position_stack)
 
 
 
@@ -42,7 +44,7 @@ func _on_cactus_target_mouse_exited():
 	# Set the speed and direction of the particle
 	var speed = 600
 	var direction = Vector2(mouse_position_stack[0].direction_to(mouse_position_stack[4]))
-	particle.linear_velocity = direction * speed
+	particle.linear_velocity = direction * speed * randf_range(.5, 1.5)
 	
 	# Spawn the particle
 	add_child(particle)
@@ -51,11 +53,22 @@ func _on_cactus_target_mouse_exited():
 	$CactusTarget/Sprite2D.texture = load("res://Sprites/Cactus0%s.svg" % str(randi_range(1, 5)))
 
 
-func _on_save_game_pressed() -> void:
-	save_game()
 
+
+# Saves the variables in save_data.gd to a save file
 func save_game():
 	ResourceSaver.save(saveData, save_file)
 
+# Loads the variables from a save file to save_data.gd
 func load_game():
 	saveData = ResourceLoader.load(save_file)
+
+
+# Save the game every 30 seconds
+func _on_save_timer_timeout() -> void:
+	save_game()
+
+
+# Save the game just before closing
+func _on_tree_exiting() -> void:
+	save_game()
